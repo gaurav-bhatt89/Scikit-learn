3baf63379ed0dd6f0632692cb251b7ff34d37eae
Gaurav Bhatt <rajgorgm@gmail.com>
2025-05-06 19:28:27 +0530

    Update

---
+++ K_Means_Clustering_Blob_Tables.py
@@ -0,0 +1,7 @@
+## Lets import necessary python libraries
+import numpy as np # Numerical Python library used to perform numerical operations
+import pandas as pd # Pandas library used to handle and analyze structured data
+import matplotlib.pyplot as plt # Plotting library
+import seaborn as sns # Additional plotting library built over matplotlib (better visuals)
+import string
+sns.set_theme(style='darkgrid',palette='Set2')
